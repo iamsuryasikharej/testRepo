40c4f26c6f7d2ebfbb68875d97f13b7bf7ddddbd
imsuryasikharej <iamssrofficial@gmail.com>
2021-02-16 14:11:27 +0530

    first commit

---
+++ abc.java
@@ -0,0 +1,13 @@
+package com.surya.test;
+class abcd
+{
+	public static void main(String[] args) {
+		System.out.print("abcd");
+	}
+}
+class abcde
+{
+	public static void main(String[] args) {
+		System.out.print("abcde");
+	}
+}
